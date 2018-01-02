5cc6badd563f1b93e0c35c1c62e39f03ecfbd59b
Xerik <dsshar@gmail.com>
2018-01-02 09:26:35 -0500

    implementing small dialogue feature

---
+++ Assets/Scripts/ClassroomPerson.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClassroomPerson : MonoBehaviour {
+
+	public AudioSource greetAudio;
+	public AudioSource goodbyeAudio;
+	public Canvas speechBubble;
+
+	// Use this for initialization
+	void Start () {
+		print (speechBubble.GetComponentInChildren<Button> ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Talk() {
+		greetAudio.Play ();
+		speechBubble.enabled = true;
+	}
+
+	public void Goodbye() {
+		goodbyeAudio.Play ();
+		speechBubble.enabled = false;
+	}
+}
---
+++ Assets/Scripts/SpeechButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechButton : MonoBehaviour {
+
+	ClassroomPerson test;
+	public static event System.Action close;
+
+	// Use this for initialization
+	void Start () {
+		print (GetComponentInParent<ClassroomPerson> ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void closeButton() {
+
+	}
+}
