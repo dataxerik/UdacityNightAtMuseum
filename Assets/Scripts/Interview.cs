76c4590ad94736f179eb54214100e27ee9962871
dsharp <dsshar@gmail.com>
2018-01-03 11:48:47 -0500

    updates to various scenes

--- Assets/Scripts/Interview.cs
+++ Assets/Scripts/Interview.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interview : MonoBehaviour {
 
 	public AudioSource interview;
+	public Text buttonText;
+	string playText = "Play interview";
+	string stopText = "Stop interview";
 
 	public void PlayInterview() {
 		if (!interview.isPlaying) {
 			interview.Play ();
+			buttonText.text = stopText;
+		} else {
+			interview.Stop ();
+			buttonText.text = stopText;
 		}
 	}
 }
