ad71e866ecf0dd30c66ef2036a07b942a1419da6
dsharp <dsshar@gmail.com>
2018-01-02 19:14:41 -0500

    more updates to thereapy scene

---
+++ Assets/Scripts/Interview.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interview : MonoBehaviour {
+
+	public AudioSource interview;
+
+	public void PlayInterview() {
+		if (!interview.isPlaying) {
+			interview.Play ();
+		}
+	}
+}
