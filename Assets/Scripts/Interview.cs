c33ca1d09d0681b2f1bfa693129fc7fc8e791aa0
dsharp <dsshar@gmail.com>
2018-01-08 11:09:00 -0500

    added interview to thereapy scene

--- Assets/Scripts/Interview.cs
+++ Assets/Scripts/Interview.cs
@@ -7,16 +7,28 @@ public class Interview : MonoBehaviour {
 
 	public AudioSource interview;
 	public Text buttonText;
+	bool isPlaying = false;
 	string playText = "Play interview";
-	string stopText = "Stop interview";
+	string stopText = "Pause interview";
+
+	void Update() {
+		updateAudioDisplayText ();
+	}
+
+	void updateAudioDisplayText() {
+		if (buttonText.text.Equals (stopText) && !interview.isPlaying) {
+			buttonText.text = playText;
+		}
+	}
 
 	public void PlayInterview() {
 		if (!interview.isPlaying) {
 			interview.Play ();
 			buttonText.text = stopText;
 		} else {
-			interview.Stop ();
-			buttonText.text = stopText;
+			interview.Pause ();
+			print (interview.time);
+			buttonText.text = playText;
 		}
 	}
 }
